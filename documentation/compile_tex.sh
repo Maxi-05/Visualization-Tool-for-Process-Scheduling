a95e12c4a2093127f05658911d3af791a629f3f4
krthk-22 <cs22b026@smail.iitm.ac.in>
2024-11-14 16:50:05 +0530

    Created template for weekly task update

---
+++ documentation/compile_tex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compile the LaTeX document to PDF
+read -p "Enter the name of the LaTeX file (without extension): " tex_file
+tex_file_name="${tex_file}.tex"
+pdflatex -shell-escape $tex_file_name
+
+# Clean up auxiliary files
+rm -rf _minted-${tex_file}
+rm ${tex_file}.aux ${tex_file}.log ${tex_file}.toc ${tex_file}.out ${tex_file}.lof texput.log
--- documentation/compile_time.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile the LaTeX document to PDF
-pdflatex -shell-escape $tex_file_name
-
-# Clean up auxiliary files
-rm report.aux report.log report.toc report.out report.lof texput.log
